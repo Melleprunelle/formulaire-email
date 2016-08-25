ea5edf5aa6e26e2a5ae3215aa3835718b8c2a407
Lise Poirier <lise.p.poirier@gmail.com>
2016-08-25 14:51:04 +0200

    Mise en place de l'envois des informations

--- email.php
+++ email.php
@@ -1,3 +1,12 @@
 <?php
 
+//$identite = $_POST['idendite'];
+$email = $_POST['email'];
+$objet = $_POST['objet'];
+$message = $_POST['message'];
+$expediteur = 'from : adressebidon@gmail.com';
+
+mail ($email, $objet, $message, $expediteur);
+echo "Votre message à bien été envoyé.";
+    
 ?>
--- index.php
+++ index.php
@@ -11,40 +11,40 @@
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <body>
 <div class="container">
-
-    <h2 class="col-md-offset-4">Nouveau message</h2>
     
-    <div class="col-md-offset-1 col-md-3">
-                <img class="" src="img/envelope.svg" alt="Enveloppe Image">
-            </div>
+    <div class="col-md-offset-1 col-sm-offset-1 col-md-3 col-sm-3">
+        <img class="img-responsive" src="img/envelope.svg" alt="Enveloppe Image">
+    </div>
+    
+    <h2 class="text-center-xs col-md-offset-4 col-sm-offset-4">Nouveau message</h2>
     
     <form class="" action="email.php" method="post">
             
-           <div class="form-group col-md-7">
+           <div class="form-group col-md-7 col-sm-7">
                 <label for="usr">Nom Prénom :</label>
-                <input type="text" class="form-control" id="usr" placeholder="Nom et prénom">
+                <input type="text" class="form-control" id="usr" placeholder="Nom et prénom" name="idendite">
             </div>
 
-            <div class="form-group col-md-7">
+            <div class="form-group col-md-7  col-sm-7">
                 <label for="email">E-mail :</label>
-                <input type="email" class="form-control" id="email" placeholder="E-mail">
+                <input type="email" class="form-control" id="email" placeholder="E-mail" name="email">
             </div>
 
-            <div class="form-group col-md-7">
+            <div class="form-group col-md-7  col-sm-7">
                 <label for="objet">Objet :</label>
-                <input type="text" class="form-control" id="objet" placeholder="Objet">
+                <input type="text" class="form-control" id="objet" placeholder="objet" name="objet">
             </div>
 
-            <div class="form-group col-md-7">
+            <div class="form-group col-sm-offset-4 col-md-7  col-sm-7">
                 <label for="message">Message :</label>
                 <textarea class="form-control" name="message" id="comment" cols="30" rows="8" placeholder="Message"></textarea>
             </div>
 
-            <div class="form-group col-md-offset-4 col-md-7">
+            <div class="form-group col-md-offset-4 col-sm-offset-4 col-md-7  col-sm-7">
                 <input id="input-repl-2" type="file" class="file-loading" accept="image/*">
             </div>
 
-            <div class="form-group col-md-offset-4  col-md-7">
+            <div class="form-group col-md-offset-4  col-sm-offset-4 col-md-7 col-sm-7">
                 <button type="submit" class="btn btn-primary">Envoyer</button>
             </div>
     </form>
