7ee96c9377c91823f93d8fd45041c2fcf9c3b26f
Lise Poirier <lise.p.poirier@gmail.com>
2016-08-25 17:21:27 +0200

    Mise en place responsive img

--- confirmation.php
+++ confirmation.php
@@ -12,42 +12,11 @@
 <body>
 <div class="container">
     
-    <div class="col-md-offset-1 col-sm-offset-1 col-md-3 col-sm-3">
-        <img class="img-responsive" src="img/envelope.svg" alt="Enveloppe Image">
+    <div class="col-md-2 col-md-offset-5 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+        <img class="img-responsive confirm" src="img/envelope.svg" alt="Enveloppe Image">
     </div>
     
-    <h2 class="text-center-xs col-md-offset-4 col-sm-offset-4">Votre message a bien été envoyé.</h2>
-    
-    <form class="" action="email.php" method="post">
-            
-           <div class="form-group col-md-7 col-sm-7">
-                <label for="usr">Nom Prénom :</label>
-                <input type="text" class="form-control" id="usr" placeholder="Nom et prénom" name="idendite" maxlength="50">
-            </div>
-
-            <div class="form-group col-md-7  col-sm-7">
-                <label for="email">E-mail :</label>
-                <input type="email" class="form-control" id="email" placeholder="E-mail" name="email" maxlength="50">
-            </div>
-
-            <div class="form-group col-md-7  col-sm-7">
-                <label for="objet">Objet :</label>
-                <input type="text" class="form-control" id="objet" placeholder="objet" name="objet" maxlength="50">
-            </div>
-
-            <div class="form-group col-sm-offset-4 col-md-7  col-sm-7">
-                <label for="message">Message :</label>
-                <textarea class="form-control" name="message" id="comment" cols="30" rows="8" maxlength="750" placeholder="Message"></textarea>
-            </div>
-
-            <div class="form-group col-md-offset-4 col-sm-offset-4 col-md-7  col-sm-7">
-                <input id="input-repl-2" type="file" class="file-loading" accept="image/*" name="fichier">
-            </div>
-
-            <div class="form-group col-md-offset-4  col-sm-offset-4 col-md-7 col-sm-7">
-                <button type="submit" class="btn btn-primary">Envoyer</button>
-            </div>
-    </form>
+    <h2 class="col-md-12 col-sm-12 col-xs-12 text-center">Votre message a bien été envoyé.</h2>
 
 </div>
 </body>
